f642fd2599ee50adba03d786a88439d8e57fd2bf
StevenVallarsa <orionsjv@gmail.com>
2019-04-29 02:03:11 -0400

    added method to check for numbers in words

--- Capstone-01-PigLatin/Program.cs
+++ Capstone-01-PigLatin/Program.cs
@@ -20,15 +20,35 @@ namespace Capstone_01_PigLatin
                     input = "z";
                 }
 
-                string output = PigLatin(input);
+                // check for numeric digits and symbols
+                bool isAlpha = CheckNumeric(input);
 
+                string output = " ";
+
+                // if no numbers in word run Method to turn into Pig Latin
+                // if numbers are detected, make output string "N/A" to be caught in the next step
+                if (isAlpha)
+                {
+                   output = PigLatin(input);
+                }
+                else
+                {
+                    output = "N/A";
+                }
+
+                // Pig Latin Method will return " " if there are no vowels
                 if (output == " ")
                 {
                     Console.WriteLine("Oops. That can't be converted.\n");
                 }
+                else if (output == "N/A")
+                {
+                    Console.WriteLine("Your word contains non-letter characters and can't be converted.\n");
+                }
                 else
                 {
-                    Console.WriteLine($"{char.ToUpper(input[0]) + input.Substring(1)} is {output} in pig Latin.\n");
+                    //Console.WriteLine($"{char.ToUpper(input[0]) + input.Substring(1)} is {output} in pig Latin.\n");
+                    Console.WriteLine($"{input.ToUpper()} is {output.ToUpper()} in pig Latin.\n");
                 }
 
                 Console.Write("Would you like to try another word? (y/n) ");
@@ -52,7 +72,7 @@ namespace Capstone_01_PigLatin
 
             for (int i = 0; i < word.Length; i++)
             {
-
+                // itterate through each letter and check for vowels
                 if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
                 {
                     if (i == 0)
@@ -69,6 +89,8 @@ namespace Capstone_01_PigLatin
             return " ";
 
         }
+
+
 
         public static bool CheckExit()
         {
@@ -94,6 +116,21 @@ namespace Capstone_01_PigLatin
                 Console.Write("Sorry, I couldn't understand you. Please try again: ");
             }
 
+        }
+
+
+        // check for numbers in word and not convert it to Pig Latin
+        public static bool CheckNumeric(string input)
+        {
+
+            foreach (char digit in input)
+            {
+                if (char.IsDigit(digit))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
